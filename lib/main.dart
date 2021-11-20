458b9c8843534a84184f69cb87c175afa00fdd86
Ayush Kulshrestha <ayushkulshrestha2001@gmail.com>
2021-11-20 13:40:58 +0530

    app completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    void playSound(int n) {
+      final player = AudioCache();
+      player.play('note$n.wav');
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: [
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playSound(1);
+                  },
+                  child: Container(
+                    margin: EdgeInsets.all(0.0),
+                    color: Colors.deepOrange,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playSound(2);
+                  },
+                  child: Container(
+                    color: Colors.blueAccent,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playSound(3);
+                  },
+                  child: Container(
+                    color: Colors.deepPurpleAccent,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playSound(4);
+                  },
+                  child: Container(
+                    color: Colors.teal,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playSound(5);
+                  },
+                  child: Container(
+                    color: Colors.yellowAccent,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playSound(6);
+                  },
+                  child: Container(
+                    color: Colors.black12,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playSound(7);
+                  },
+                  child: Container(
+                    color: Colors.amber,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+// Center(
+// child: TextButton(
+// onPressed: () {
+// final player = AudioCache();
+// player.play('note2.wav');
+// },
+// child: Text('Click ME'),
+// ),
+// ),
